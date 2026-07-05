5aa2fefdbbd3261d74badab6af923856dab078fd
bjakbn <bjak6286@gmail.com>
2026-07-06 05:52:56 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
-
+// 文件位置：document/settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolution {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "DocTree"
+include(":app")
